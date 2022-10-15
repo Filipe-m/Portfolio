6176d4502b7f77488255c8fa7315cd0cc48402f0
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2022-10-15 15:39:00 -0300

    Header done

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  import HeaderContent from './lib/header.svelte'
+</script>
+
+<main>
+  <header>
+    <HeaderContent />
+  </header>
+  <article>
+    <div class="custom-shape-divider-top-1665858180">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+      </svg>
+  </div>
+    <section />
+  </article>
+</main>
+
+<style>
+  main{
+    position: relative;
+  }
+  article{
+    position: relative;
+    background-color: rgb(12, 109, 119);
+    height: 100vh;
+  }
+  header {
+    display: flex;
+    flex-direction: row-reverse;
+    height: 100vh;
+    width: 100vw;
+    align-items: center;
+    justify-content: space-between;
+    background-image: url(./assets/headerBackground.svg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  @media only screen and (max-width: 600px) {
+    header {
+      flex-direction: column;
+      justify-content: space-around;
+      background-image: url(./assets/headerBackgroundMobile.svg);
+    }
+  }
+</style>
---
+++ src/lib/header.svelte
@@ -0,0 +1,152 @@
+<script>
+  import linkedinLogo from 'C:/Users/aikad/Desktop/Projects/Portfolio/src/assets/linkedin.svg'
+  import emailLogo from 'C:/Users/aikad/Desktop/Projects/Portfolio/src/assets/email.svg'
+  import githubLogo from 'C:/Users/aikad/Desktop/Projects/Portfolio/src/assets/github.svg'
+  import instagramLogo from 'C:/Users/aikad/Desktop/Projects/Portfolio/src/assets/instagram.svg'
+  let contact = [
+    {
+      link: 'https://github.com/Filipe-m',
+      icon: githubLogo,
+      name: 'Github'
+    },
+    {
+      link: 'mailto:liebeoxygen@gmail.com',
+      icon: emailLogo,
+      name: 'Email'
+    },
+    {
+      link: 'https://www.linkedin.com/in/filipe-moreira-c',
+      icon: linkedinLogo,
+      name: 'Linkedin'
+    },
+    {
+      link: 'https://www.instagram.com/filipe.m_/',
+      icon: instagramLogo,
+      name: 'Instragram'
+    }
+  ]
+</script>
+
+<div class="about">
+  <img
+    class="profilePic"
+    src="https://avatars.githubusercontent.com/u/60034638?v=4"
+    alt=""
+  />
+  <div class="icons">
+    {#each contact as site}
+      <span>
+        <a class="icon" href={site.link}
+          ><img src={site.icon} class="img" alt={site.name} /></a
+        >
+      </span>
+    {/each}
+  </div>
+</div>
+<div class="article">
+  <h1>Hi, I'm Filipe Moreira</h1>
+  <div class="text">
+    <p>
+      I'm currently a last-year highschool student who's trying to learn web
+      development
+    </p>
+    <p>
+      Tecnologies I'm able to use: <br />
+      <code>React <br /> Svelte <br /> Javascript</code>
+    </p>
+  </div>
+</div>
+
+<style>
+  code {
+    color: rgb(30, 0, 255);
+    font-size: 130%;
+  }
+
+  .text {
+    font-size: 120%;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .text p{
+    margin: 3%;
+    text-align: center;
+  }
+
+  .article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 60%;
+    height: 100%;
+  }
+
+  .article h1 {
+    font-size: 300%;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  .about {
+    width: 40%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2%;
+  }
+
+  .profilePic {
+    width: 50%;
+    border-radius: 50%;
+  }
+
+  .icons {
+    width: 50%;
+    height: fit-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 7%;
+  }
+
+  .icons span {
+    width: 90%;
+    height: 90%;
+  }
+
+  .icons span a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .img {
+    max-width: 100%;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .about {
+      height: 40vh;
+      width: 100vw;
+      justify-content: flex-end;
+    }
+    .article {
+      align-items: center;
+      justify-content: center;
+      height: 60vh;
+      width: 100vw;
+    }
+    .article h1 {
+      font-size: 180%;
+    }
+    .text {
+      text-align: center;
+      margin-left: 10%;
+      margin-right: 10%;
+    }
+  }
+</style>
